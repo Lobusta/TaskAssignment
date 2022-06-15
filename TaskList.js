ae4f54403d69c6cdae2e09c58fb084a6ffcafdea
Lobusta <sajid.ahmad.1731998@gmail.com>
2022-06-15 16:26:12 +0500

    modified:   TaskList.css 	modified:   TaskList.html 	modified:   TaskList.js

--- TaskList.js
+++ TaskList.js
@@ -12,38 +12,39 @@ document.getElementById("submitbutton").addEventListener("click", function(event
 function AddTask(){
 
     var newtask=document.getElementById('create-task').value;
+    document.getElementById('create-task').innerHTML = "";
     
-    if (newtask != null) {
-        console.log(newtask);
+    if (newtask == ""){
+        alert("Please fill the form");
     }
-    else{
-        console.log("this bitch null");
+    else {
+        Addnewtask(newtask);
     }
-    
-    Addnewtask(newtask);
 
 
-    function Addnewtask() {
-        const div = document.createElement('div');
-      
-        div.className = arguments[0];
-      
-        div.innerHTML = `
-                        <div class="task-list-element">
-                            <div class="check-mark-container">
-                                  <img class="check-mark" src="icon-check.svg"/>
-                             </div>
-                            <div class="task-list-element-inside">
-                                  <p>`+arguments[0]+`</p>
-                                  <img onclick="delete_row(this)" class="cross" src="icon-cross.svg"/>
-                             </div>
-                        </div>
-        `;
-        console.log("we have reached here");
-        document.getElementById('task-list-container').appendChild(div);
-      
-      }
 }
+
+
+function Addnewtask() {
+    const div = document.createElement('div');
+  
+    div.className = arguments[0];
+  
+    div.innerHTML = `
+                    <div class="task-list-element">
+                        <div onclick="this.classList.toggle('check-mark-container-active')" class="check-mark-container">
+                              <img  class="check-mark" src="icon-check.svg"/>
+                         </div>
+                        <div class="task-list-element-inside">
+                              <p>`+arguments[0]+`</p>
+                              <img onclick="delete_row(this)" class="cross" src="icon-cross.svg"/>
+                         </div>
+                    </div>
+    `;
+
+    document.getElementById('task-list-container').appendChild(div);
+    
+  }
 
 
     function delete_row(e)
@@ -53,3 +54,8 @@ function AddTask(){
 
 
 
+    
+    
+
+        
+    
